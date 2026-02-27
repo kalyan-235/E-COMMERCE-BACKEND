948c9c09fc32ccf06be93cf62e012a62fc3169d3
kalyan-235 <somarapukalyanram@gmail.com>
2026-02-27 19:08:29 +0530

    Initial backend commit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const errorHandler = require("./middleware/error.middleware");
 
 const app = express();
 
-app.use(cors({ origin:[process.env.FRONTEND_URL,"http://localhost:5173"], credentials: true }));
+app.use(cors({ origin:["http://localhost:5173","https://e-commerce-web-application-wgrp.onrender.com"], credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
